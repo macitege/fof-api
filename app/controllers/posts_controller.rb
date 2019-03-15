52b35731f021fd63d3486f7f4e1eb16cb6af3500
macitege <macitegeercan@gmail.com>
2019-03-14 21:30:00 -0400

    Make users unable to edit others resources

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < OpenReadController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
